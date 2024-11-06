08c48a01a7e4936e95cb7031d48a1b068523a44e
ProgrammerFromSweden <albinhasanaj06@hotmail.com>
2024-11-06 09:54:00 +0100

    refactor

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,16 @@
+"use client";
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Divider from './Divider'
+import { usePathname } from 'next/navigation';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+    const currentPath = usePathname()
+
+
+    const names = ['Albin', 'Marcus', 'Simon', 'Elias', 'Jim', 'Oscar', 'Leo', 'Axel']
+
     return (
         <div>
             <nav className='flex flex-col gap-4 mt-4'>
@@ -15,38 +22,31 @@ const Navbar = () => {
                         height={32}
                         className='mt-1'
                     />
-                    <h1 className='text-[24px] font-bold text-secondary'>Tjarra Angar</h1>
+                    <h1 className='text-xl md:text-2xl font-bold text-secondary'>Tjarra Angar</h1>
                 </Link>
                 <div>
-                    <ul className='flex gap-14 justify-center'>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Albin</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Marcus</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Simon</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Elias</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Jim</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Oscar</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Leo</li>
-                        </Link>
-                        <Link href='/'>
-                            <li className='text-[#fff] text-[16px] font-semibold cursor-pointer hover:underline'>Axel</li>
-                        </Link>
+                    <ul className='flex flex-wrap gap-4 md:gap-14 justify-center'>
+                        {names.map((name) => {
+                            const namePath = `/${name.toLowerCase()}`
+                            const isActive = currentPath === namePath
+
+                            return (
+                                <li key={name}>
+                                    <Link href={namePath}>
+                                        <span
+                                            className={`text-base md:text-lg font-semibold cursor-pointer hover:underline ${isActive ? 'text-white' : 'text-gray-500'
+                                                }`}
+                                        >
+                                            {name}
+                                        </span>
+                                    </Link>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
                 <Divider />
-            </nav >
+            </nav>
         </div>
     )
 }
